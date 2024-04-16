682d0321c33c36557ea2ef72a657ece1a9d6ea0f
Ali Fahs <alijawadfahsfr@gmail.com>
2024-04-16 08:48:01 +0200

    small  kube fix

--- nebulous/install-kube-u22-wg.sh
+++ nebulous/install-kube-u22-wg.sh
@@ -89,6 +89,7 @@ sudo docker -v || { log_print ERROR "Docker installation failed!"; exit $EXITCOD
 
 # Adding Kubernetes Repo
 log_print INFO "Adding Kubernetes Repo"
+sudo mkdir -p /etc/apt/keyrings
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.26/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 # Check for lock
